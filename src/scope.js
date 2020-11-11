39c32c20dc2bea2da1922c4791d031b05fc70133
Dave Yoon <daveyoon64@gmail.com>
2020-11-10 19:26:51 -0800

    removing test files and adding first scope

---
+++ src/scope.js
@@ -0,0 +1,28 @@
+'use strict'
+
+var _ = require('lodash');
+
+function Scope() {
+  this.$$watchers = [];
+  Scope.prototype.$watch = function(watchFn, listenerFn) {
+    var watcher = {
+      watchFn: watchFn,
+      listenerFn: listenerFn
+    };
+    this.$$watchers.push(watcher);
+  };
+  Scope.prototype.$digest = function() {
+    var self = this;
+    var oldValue, newValue;
+    _.forEach(this.$$watchers, function(watcher) {
+      newValue = watcher.watchFn(self);
+      oldValue = watcher.last;
+      if (newValue !== oldValue) {
+        watcher.last = newValue;
+        watcher.listenerFn(newValue, oldValue, self);
+      }
+    });
+  }
+}
+
+module.exports = Scope;
